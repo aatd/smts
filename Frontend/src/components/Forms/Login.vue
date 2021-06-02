3c701c1394b2f4c8b9288e410231b374b1d3ec9f
Asef Alper Tunga Dündar <asaf93@hotmail.de>
2021-06-02 09:29:20 +0200

    Changed a bit of he layout part 1

--- Frontend/src/components/Forms/Login.vue
+++ Frontend/src/components/Forms/Login.vue
@@ -1,80 +1,74 @@
 <template>
-  <div class="card login" :class="[type && `card-${type}`]">
-    <div class="card-image" v-if="$slots.image">
-      <slot name="image"></slot>
-    </div>
-    <div
-      class="card-header"
-      v-if="$slots.header || title"
-      :class="headerClasses"
-    >
-      <slot name="header">
-        <h4 class="card-title">{{ title }}</h4>
-      </slot>
-      <p class="card-category" v-if="subTitle">{{ subTitle }}</p>
-    </div>
-    <div class="card-body">
-      <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-        <!--Email or Name-->
-        <b-form-group
-          id="input-group-1"
-          label="Email address or Username:"
-          label-for="input-1"
-          description="We'll never share your email with anyone else."
+  <!--User Information-->
+  <card class="card-user login">
+    <!--Background Picture-->
+    <img
+      slot="image"
+      src="https://verbraucherfenster.hessen.de/sites/verbraucherfenster.hessen.de/files/styles/article_image/public/AdobeStock_173691120.jpeg?itok=w0s4Prt4&c=a03f68c192f0719dbcd708609666a0ea"
+      alt="..."
+    />
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <!--Email or Name-->
+      <b-form-group
+        id="input-group-1"
+        label="Email address or Username:"
+        label-for="input-1"
+        description="We'll never share your email with anyone else."
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.emailOrName"
+          type="email"
+          placeholder="Enter email"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <!--Password-->
+      <b-form-group
+        id="input-group-2"
+        label="Your Password:"
+        label-for="input-2"
+      >
+        <b-form-input
+          id="input-2"
+          type="password"
+          v-model="form.pwd"
+          placeholder="Enter Password"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <!--Remeber me-->
+      <b-form-group id="input-group-4" v-slot="{ ariaDescribedby }">
+        <b-form-checkbox-group
+          v-model="form.checked"
+          id="checkboxes-4"
+          :aria-describedby="ariaDescribedby"
         >
-          <b-form-input
-            id="input-1"
-            v-model="form.emailOrName"
-            type="email"
-            placeholder="Enter email"
-            required
-          ></b-form-input>
-        </b-form-group>
+          <b-form-checkbox value="rememberMe">Remeber me</b-form-checkbox>
+        </b-form-checkbox-group>
+      </b-form-group>
 
-        <!--Password-->
-        <b-form-group
-          id="input-group-2"
-          label="Your Password:"
-          label-for="input-2"
-        >
-          <b-form-input
-            id="input-2"
-            type="password"
-            v-model="form.pwd"
-            placeholder="Enter Password"
-            required
-          ></b-form-input>
-        </b-form-group>
-
-        <!--Remeber me-->
-        <b-form-group id="input-group-4" v-slot="{ ariaDescribedby }">
-          <b-form-checkbox-group
-            v-model="form.checked"
-            id="checkboxes-4"
-            :aria-describedby="ariaDescribedby"
-          >
-            <b-form-checkbox value="rememberMe">Remeber me</b-form-checkbox>
-          </b-form-checkbox-group>
-        </b-form-group>
-
-        <b-button type="submit" variant="primary">Submit</b-button>
-        <b-button type="reset" variant="danger">Reset</b-button>
-      </b-form>
-      <b-card class="mt-3" header="Form Data Result">
-        <pre class="m-0">{{ form }}</pre>
-      </b-card>
-    </div>
-    <slot name="raw-content"></slot>
-    <div class="card-footer" :class="footerClasses" v-if="$slots.footer">
-      <slot name="footer"></slot>
-    </div>
-  </div>
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+    <b-card class="mt-3" header="Form Data Result" v-if="$IsDebug">
+      <pre class="m-0">{{ form }}</pre>
+    </b-card>
+    <!--Device Information-->
+    <div class="author"></div>
+  </card>
 </template>
 
 <script>
+import Card from "src/components/Cards/Card.vue";
+
 export default {
   name: "login-card",
-  components: {},
+  components: {
+    Card,
+  },
   data() {
     return {
       form: {
@@ -139,8 +133,9 @@ export default {
   display: flex;
   align-items: center;
   max-width: 300px;
-  max-height: 600px;
-  margin: 25% auto;
+  height: 60vh;
+  margin: auto;
+  padding-top: 5%;
   float: none;
 }
 </style>
--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -22,16 +22,16 @@ import App from './App.vue'
 import LightBootstrap from './light-bootstrap-main'
 Vue.use(LightBootstrap)
 
-// Install Vue-Router 
+// Install Vue-Router
 import VueRouter from 'vue-router'
-import routes    from './routes/routes'
+import routes from './routes/routes'
 Vue.use(VueRouter)
 const router = new VueRouter({
-  routes:          routes,
+  routes: routes,
   linkActiveClass: 'nav-item active',
   scrollBehavior: (to) => {
     if (to.hash) {
-      return {selector: to.hash}
+      return { selector: to.hash }
     } else {
       return { x: 0, y: 0 }
     }
@@ -46,11 +46,11 @@ import './registerServiceWorker'
 import { LMap, LTileLayer, LMarker, LTooltip, LPopup } from 'vue2-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { Icon } from 'leaflet';
-Vue.component('l-map',        LMap      );
+Vue.component('l-map', LMap);
 Vue.component('l-tile-layer', LTileLayer);
-Vue.component('l-marker',     LMarker   );
-Vue.component('l-tooltip',    LTooltip  );
-Vue.component('l-popup',      LPopup    );
+Vue.component('l-marker', LMarker);
+Vue.component('l-tooltip', LTooltip);
+Vue.component('l-popup', LPopup);
 delete Icon.Default.prototype._getIconUrl;
 Icon.Default.mergeOptions({
   iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
@@ -65,6 +65,7 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
+Vue.prototype.$IsDebug = false;
 
 // MAIN-Entrypoint!
 new Vue({
