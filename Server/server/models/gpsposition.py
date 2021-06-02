19dbff9518cf718ca92121644f392a3fb61cb20a
niklasonfire <niklas200291@yahoo.de>
2021-06-02 17:50:23 +0200

    created gpsPostion class

--- Server/server/models/gpsposition.py
+++ Server/server/models/gpsposition.py
@@ -1,34 +1,28 @@
 import datetime
 import json
 
-import pymongo
-
-############### Establish connection to database ###########
-try:
-    mongo = pymongo.MongoClient(
-        "localhost:27017",
-        serverSelectionTimeoutMS=1000)
-
-    db = mongo.smts
-   # mongo.server_info()
-
-except pymongo.errors.ConnectionFailure:
-    print("Cannot connect to database")
-
-####################### coordinate class ########################
-
 
 class gpsPosition:
+
     latitude: float
     longitude: float
     height: float
     time: datetime.datetime
     velocity: float
 
-    def get_coord_as_dict(self):
+    def __init__(self, latitude=0.0, longitude=0.0,
+                 height=0.0, time=datetime.datetime(1, 1, 1, 1, 1, 1), velocity=0.0):
+        self.latitude = latitude
+        self.longitude = longitude
+        self.height = height
+        self.time = time
+        self.velocity = velocity
+
+    def as_dict(self):
         return {
-            "lat": self.lat,
-            "long": self.lon,
-            "speed": self.speed,
-            "time": self.time
+            "latitude": self.latitude,
+            "longitude": self.longitude,
+            "height": self.height,
+            "time": self.time.strftime("%m/%d/%Y %H:%M:%S"),
+            "velocity": self.velocity
         }
