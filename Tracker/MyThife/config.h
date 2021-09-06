3999e994060bd4596d16bae665a5e74770195096
Tom <bechtloff@bewegtbildhelden.de>
2021-09-06 13:35:19 +0200

    NewUpload_FunctioningParts

--- Tracker/MyThife/config.h
+++ Tracker/MyThife/config.h
@@ -1,5 +1,6 @@
-#define APN "internet.eplus.de"
-#define USER "eplus"
+#define APN      "internet.eplus.de"
+#define USER     "eplus"
 #define PASSWORD "internet"
-#define PIN { '4', '1', '3', '3' }
-#define URL "intern.bewegtbildhelden.de/"
+#define PIN      {'4','1','3','3'}
+#define OWNER    "+491702196369"
+#define UPLOADURL      "http://d5f1-2a02-908-1220-e460-e65f-1ff-fe38-dc49.ngrok.io/backend"
